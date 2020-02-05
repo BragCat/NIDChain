4fdf8df0757a798a99b0679565210093d57c98b4
bragcat <bragcat.li@gmail.com>
2020-02-05 19:51:40 +0800

    Add NIDAdmin ownership management, change organization creation to apply and review process

--- contracts/NIDAdmin.sol
+++ contracts/NIDAdmin.sol
@@ -1,26 +1,77 @@
 pragma solidity >0.4.23 <0.7.0;
 
 import "./NIDOrganization.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract NIDAdmin {
+contract NIDAdmin is Ownable {
+    NIDOrganization[] private _applications;
     NIDOrganization[] private _organizations;
 
-    event OrganizationCreated(uint256 indexed index, string indexed name, uint256 timestamp);
+    event OrganizationApplied(uint256 indexed index, string indexed name, uint256 timestamp);
+    event OrganizationDeleted(uint256 indexed index, string indexed name, uint256 timestamp);
+    event ApplicationApproved(uint256 indexed index, string indexed name, uint256 timestamp);
+    event ApplicationRejected(uint256 indexed index, string indexed name, uint256 timestamp);
 
     constructor() public {
 
     }
 
-    function createNIDOrganization(string memory _name) public {
+    // used for each NID organization admin to apply for a new organization's joining
+    function applyNIDOrganization(string memory _name) public {
         NIDOrganization org = new NIDOrganization(
             _name,
             msg.sender
         );
+        _applications.push(org);
+        emit OrganizationApplied(_applications.length - 1, _name, block.timestamp);
+    }
+
+    // used for NID system admin to approve organization application
+    function approveNIDOrganizationApplication(uint256 index) public onlyOwner {
+        require(index < _applications.length);
+        NIDOrganization org = _applications[index];
         _organizations.push(org);
-        emit OrganizationCreated(_organizations.length - 1, _name, block.timestamp);
+        removeApplication(index);
+        emit ApplicationApproved(index, org.name(), block.timestamp);
+    }
+
+    // used for NID system admin to reject organization application
+    function rejectNIDOrganizationApplication(uint256 index) public onlyOwner {
+        require(index < _applications.length);
+        string memory name = _applications[index].name();
+        removeApplication(index);
+        emit ApplicationRejected(index, name, block.timestamp);
+    }
+
+    // used for NID system admin to delete obsolete organizations
+    function deleteNIDOrganization(uint256 index) public onlyOwner{
+        require(index < _organizations.length);
+        string memory name = _applications[index].name();
+        removeOrganization(index);
+        emit OrganizationDeleted(index, name, block.timestamp);
+    }
+
+    function getApplicationCount() view public returns(uint256) {
+        return _applications.length;
     }
 
     function getOrganizationCount() view public returns(uint256) {
         return _organizations.length;
+    }
+
+    function removeApplication(uint256 index) private {
+        for (uint256 i = index; i < _applications.length - 1; ++i) {
+            _applications[i] = _applications[i + 1];
+        }
+        delete _applications[_applications.length - 1];
+        --_applications.length;
+    }
+
+    function removeOrganization(uint256 index) private {
+        for (uint256 i = index; i < _organizations.length - 1; ++i) {
+            _organizations[i] = _organizations[i + 1];
+        }
+        delete _organizations[_organizations.length - 1];
+        --_organizations.length;
     }
 }
--- test/NIDAdmin_test.js
+++ test/NIDAdmin_test.js
@@ -6,21 +6,101 @@ contract("NIDAdmin", accounts => {
     const admin = accounts[1];
 
     describe("deployment", () => {
-        it("has been deployed", async() => {
+        beforeEach(async() => {
             NIDAdmin = await NIDAdminContract.deployed();
+        });
+        it("has been deployed", async() => {
             assert(NIDAdmin, "NID admin was not deployed successfully");
         });
     });
 
-    describe("organization creation", () => {
-        it("increments the organization count", async() => {
-            const currentCount = await NIDAdmin.getOrganizationCount();
-            await NIDAdmin.createNIDOrganization(
+    describe("organization application", () => {
+        it("increments the count and emits the event", async() => {
+            const currentCount = await NIDAdmin.getApplicationCount();
+            const tx = await NIDAdmin.applyNIDOrganization(
                 name,
                 {from: admin}
             );
-            const newCount = await NIDAdmin.getOrganizationCount();
-            assert.equal(newCount - currentCount, 1, "organization count should increment by 1");
+            const newCount = await NIDAdmin.getApplicationCount();
+            const expectedEvent = "OrganizationApplied";
+            const actualEvent = tx.logs[tx.logs.length - 1].event;
+            assert.equal(newCount - currentCount, 1, "application count should increment by 1");
+            assert.equal(actualEvent, expectedEvent, "events should match");
+        });
+    });
+
+    describe("organization review", () => {
+        beforeEach(async() => {
+            await NIDAdmin.applyNIDOrganization(
+                name,
+                {from: admin}
+            );
+            await NIDAdmin.applyNIDOrganization(
+                name,
+                {from: admin}
+            );
+        });
+        it("throws an error when not called by admin", async() => {
+            try {
+                await NIDAdmin.approveNIDOrganizationApplication(0, {from: accounts[1]});
+                assert.fail("approve should not be permitted");
+            } catch(err) {
+                const expectedError = "Ownable: caller is not the owner";
+                const actualError = err.reason;
+                assert.equal(actualError, expectedError, "should be Ownable error");
+            }
+            try {
+                await NIDAdmin.rejectNIDOrganizationApplication(0, {from: accounts[1]});
+                assert.fail("approve should not be permitted");
+            } catch(err) {
+                const expectedError = "Ownable: caller is not the owner";
+                const actualError = err.reason;
+                assert.equal(actualError, expectedError, "should be Ownable error");
+            }
+            try {
+                await NIDAdmin.deleteNIDOrganization(0, {from: accounts[1]});
+                assert.fail("approve should not be permitted");
+            } catch(err) {
+                const expectedError = "Ownable: caller is not the owner";
+                const actualError = err.reason;
+                assert.equal(actualError, expectedError, "should be Ownable error");
+            }
+        });
+        it("approves the application when called by admin", async() => {
+            const currentOrgCount = await NIDAdmin.getOrganizationCount();
+            const currentAppCount = await NIDAdmin.getApplicationCount();
+            const tx = await NIDAdmin.approveNIDOrganizationApplication(0);
+            const newOrgCount = await NIDAdmin.getOrganizationCount();
+            const newAppCount = await NIDAdmin.getApplicationCount();
+            const expectedEvent = "ApplicationApproved";
+            const actualEvent = tx.logs[tx.logs.length - 1].event;
+            assert.equal(newOrgCount - currentOrgCount, 1, "organization count should increment by 1");
+            assert.equal(newAppCount - currentAppCount, -1, "application count should decrement by 1");
+            assert.equal(actualEvent, expectedEvent, "events should match");
+        });
+        it("rejects the application when called by admin", async() => {
+            const currentOrgCount = await NIDAdmin.getOrganizationCount();
+            const currentAppCount = await NIDAdmin.getApplicationCount();
+            const tx = await NIDAdmin.rejectNIDOrganizationApplication(0);
+            const newOrgCount = await NIDAdmin.getOrganizationCount();
+            const newAppCount = await NIDAdmin.getApplicationCount();
+            const expectedEvent = "ApplicationRejected";
+            const actualEvent = tx.logs[tx.logs.length - 1].event;
+            assert.equal(newOrgCount - currentOrgCount, 0, "organization count should stay unchanged");
+            assert.equal(newAppCount - currentAppCount, -1, "application count should decrement by 1");
+            assert.equal(actualEvent, expectedEvent, "events should match");
+        });
+        it("deletes the organization when called by admin", async() => {
+            const currentOrgCount = await NIDAdmin.getOrganizationCount();
+            const currentAppCount = await NIDAdmin.getApplicationCount();
+            const tx = await NIDAdmin.deleteNIDOrganization(0);
+            const newOrgCount = await NIDAdmin.getOrganizationCount();
+            const newAppCount = await NIDAdmin.getApplicationCount();
+            const expectedEvent = "OrganizationDeleted";
+            const actualEvent = tx.logs[tx.logs.length - 1].event;
+            assert.equal(newOrgCount - currentOrgCount, -1, "organization count should decrement by 1");
+            assert.equal(newAppCount - currentAppCount, 0, "application count should stay unchanged");
+            assert.equal(actualEvent, expectedEvent, "events should match");
         });
     });
 });
